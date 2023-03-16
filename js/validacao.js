4e593b76a8e66ffa26478b0d29513e4b6d61450c
GustavoBryan <devgustavob@gmail.com>
2023-03-16 20:52:35 -0300

    js

--- js/validacao.js
+++ js/validacao.js
@@ -4,10 +4,37 @@ export function valida(input) {
   if (validadores[tipoDeInput]) {
     validadores[tipoDeInput](input);
   }
+
+  if (input.validity.valid) {
+    input.parentElement.classList.remove("input-container--invalido");
+    input.parentElement.querySelector(".input-mensagem-erro").innerHTML = "";
+  } else {
+    input.parentElement.classList.add("input-container--invalido");
+    input.parentElement.querySelector(".input-mensagem-erro").innerHTML = "";
+  }
 }
+
+const mensagensDeErro = {
+  nome: {
+    valueMissing: "O campo nome não pode estar vazio",
+  },
+  email: {
+    valueMissing: "O campo de email não pode estar vazio.",
+    valueMissing: "O email digitado não é válido.",
+  },
+  senha: {
+    valueMissing: "O campo de senha não pode estar vazio.",
+    patternMismatch:
+      "A senha deve conter entre 6 a 12 caracteres, deve conter pelo menos uma letra maiúscula, um número e não deve conter símbolos.",
+  },
+  dataNascimento: {
+    valueMissing: "O campo de nascimento não pode estar vazio.",
+  },
+};
 
 const validadores = {
   dataNascimento: (input) => validaDataNascimento(input),
+  customError: "Você deve ter mais de 18 anos, para se cadastrar!",
 };
 
 dataNascimento.addEventListener("blur", (evento) => {
